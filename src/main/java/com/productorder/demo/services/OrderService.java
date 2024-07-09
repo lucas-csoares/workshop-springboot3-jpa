fb6531a4f78f5b0d364e04c04952ec1583933193
Lucas Soares <polonio230@hotmail.com>
2024-07-09 17:59:22 -0300

    feat: Order, Instant, ISO 8601

---
+++ src/main/java/com/productorder/demo/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.productorder.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.productorder.demo.entities.Order;
+import com.productorder.demo.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+	
+}
